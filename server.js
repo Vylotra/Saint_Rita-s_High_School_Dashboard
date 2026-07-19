8a52bf4f82bda4cd47f2f27be0ee80dacc69dd94
Washak Billah <washakbillah9@gmail.com>
2026-07-19 13:17:21 +0600

    Update port for render

--- server.js
+++ server.js
@@ -59,6 +59,8 @@ app.post('/login', async (req, res) => {
 });
 
 // Server Listen on Port 5000
-app.listen(5000, () => {
-    console.log('Server is running on http://localhost:5000');
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
